d035e118dfbd6184e78d3b5ac7fcee6829f3ace8
sioncojp <sion_cojp@yahoo.co.jp>
2020-02-06 17:07:19 +0900

    log output: json

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sioncojp/sample-go-app
+
+go 1.12
+
+require go.uber.org/zap v1.13.0 // indirect
--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"sync/atomic"
 	"time"
+
+	"go.uber.org/zap"
 )
 
 type key int
+
+// Logger ... zap logger
+type Logger struct {
+	logger *zap.Logger
+	sugar  *zap.SugaredLogger
+}
 
 const (
 	requestIDKey key = 0
@@ -21,14 +28,22 @@ const (
 var (
 	listenAddr string
 	healthy    int32
+	zapLogger  Logger
 )
+
+func init() {
+	// logger初期化
+	l, _ := zap.NewProduction()
+	defer l.Sync()
+	s := l.Sugar()
+	zapLogger = Logger{l, s}
+}
 
 func main() {
 	flag.StringVar(&listenAddr, "listen-addr", ":8000", "server listen address")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-	logger.Println("Server is starting...")
+	zapLogger.sugar.Info("Server is starting...")
 
 	router := http.NewServeMux()
 	router.Handle("/", index())
@@ -38,10 +53,12 @@ func main() {
 		return fmt.Sprintf("%d", time.Now().UnixNano())
 	}
 
+	errorLog, _ := zap.NewStdLogAt(zapLogger.logger, zap.ErrorLevel)
+
 	server := &http.Server{
 		Addr:         listenAddr,
-		Handler:      tracing(nextRequestID)(logging(logger)(router)),
-		ErrorLog:     logger,
+		Handler:      tracing(nextRequestID)(logging(zapLogger.logger)(router)),
+		ErrorLog:     errorLog,
 		ReadTimeout:  5 * time.Second,
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  15 * time.Second,
@@ -53,7 +70,7 @@ func main() {
 
 	go func() {
 		<-quit
-		logger.Println("Server is shutting down...")
+		zapLogger.sugar.Info("Server is shutting down...")
 		atomic.StoreInt32(&healthy, 0)
 
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
@@ -61,19 +78,19 @@ func main() {
 
 		server.SetKeepAlivesEnabled(false)
 		if err := server.Shutdown(ctx); err != nil {
-			logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+			zapLogger.sugar.Fatalf("Could not gracefully shutdown the server: %v\n", err)
 		}
 		close(done)
 	}()
 
-	logger.Println("Server is ready to handle requests at", listenAddr)
+	zapLogger.sugar.Info("Server is ready to handle requests at", listenAddr)
 	atomic.StoreInt32(&healthy, 1)
 	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
+		zapLogger.sugar.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
 	}
 
 	<-done
-	logger.Println("Server stopped")
+	zapLogger.sugar.Info("Server stopped")
 }
 
 func index() http.Handler {
@@ -99,7 +116,7 @@ func healthz() http.Handler {
 	})
 }
 
-func logging(logger *log.Logger) func(http.Handler) http.Handler {
+func logging(logger *zap.Logger) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
@@ -107,7 +124,7 @@ func logging(logger *log.Logger) func(http.Handler) http.Handler {
 				if !ok {
 					requestID = "unknown"
 				}
-				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+				zapLogger.sugar.Infof("%s %s %s %s %s", requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
 			}()
 			next.ServeHTTP(w, r)
 		})
